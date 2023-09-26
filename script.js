aba12ff4d51641ba04020d430b82cb8feca6eb07
tastas000 <TiiLg0o000@gmail.com>
2023-09-27 01:35:28 +0700

    Fix getPlayerChoice function

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function getComputerChoice(){
 
 function getPlayerChoice(){
   let choice = Array('Rock', 'Scissors','Paper');
+  let out;
   do {
-    let out = prompt("Your choice?").toLowerCase();
+    out = prompt("Your choice?").toLowerCase();
     out = out.charAt(0).toUpperCase() + out.slice(1);
   }
   while (choice.indexOf(out) == -1);
