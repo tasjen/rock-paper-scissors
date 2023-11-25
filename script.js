6bf9e38cfa433e0503b57b6967a5619adf622765
tastas000 <TiiLg0o000@gmail.com>
2023-11-25 09:15:48 +0700

    Clean code

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const CHOICE = ['Rock', 'Scissors','Paper'];
-
-const computerChoice = document.querySelectorAll('#computer-side button');
-const playerChoice = document.querySelectorAll('#player-side button');
-const playButton = document.querySelector('#play-button');
+const choice = ['Rock', 'Paper', 'Scissors'];
 
 const computerScoreDisplay = document.querySelector('#computer-score');
 const message = document.querySelector('#message');
@@ -14,85 +10,80 @@ let computerScore = 0;
 let isFirstRound = true;
 let isEndRound = false;
 
-
-playerChoice.forEach((button) =>
+const computerChoiceButtons = document.querySelectorAll('#computer-side button');
+const playerChoiceButtons = document.querySelectorAll('#player-side button');
+playerChoiceButtons.forEach((button) =>
   button.addEventListener('click', (event) => {
     if (!isEndRound && !isFirstRound) {
-      playRound(event.target.textContent);
+      const playerChoice = event.target.textContent;
+      const computerChoice = getComputerChoice();
+      playRound(playerChoice, computerChoice);
       button.classList.add('selected-choice');
     }
   })
 );
 
+const playButton = document.querySelector('#play-button');
 playButton.addEventListener('click', () => {
-  
   if (isFirstRound) {
     resetUI(); //when clicking play again
     playButton.textContent = 'Next Round';
     isFirstRound = false;
-  }
-  else if (isEndRound) {
+  } else if (isEndRound) {
     message.textContent = 'Select Your Choice';
     isEndRound = false;
   }
-
-  playerChoice.forEach(button => button.classList.remove('selected-choice'));
-  computerChoice.forEach(button => button.classList.remove('selected-choice'));
+  playerChoiceButtons.forEach((button) => button.classList.remove('selected-choice'));
+  computerChoiceButtons.forEach((button) =>
+    button.classList.remove('selected-choice')
+  );
   playButton.style.display = 'none'; //hide this button after clicking it
+});
 
-})
-
-function getComputerChoice(){
+function getComputerChoice() {
   //randomly choose one from Rock, Scissors, and Paper
-  const choice = CHOICE[Math.floor(Math.random() * 3)]; 
-  for (let button of computerChoice){
-    if (button.textContent === choice){
+  const computerChoice = choice[Math.floor(Math.random() * 3)];
+  for (let button of computerChoiceButtons) {
+    if (button.textContent === computerChoice) {
       button.classList.add('selected-choice');
       break;
     }
   }
-  return choice;
+  return computerChoice;
 }
 
-function playRound(playerChoice){
-  const computerChoice = getComputerChoice();
-
-  if (playerChoice === computerChoice){
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
     message.textContent = `Tie! ${playerChoice} vs ${computerChoice}`;
   }
-
-  //if computerSelection is the next element of playerSelection in choice then player wins
-  else if (CHOICE[(CHOICE.indexOf(playerChoice) + 1) % 3] === computerChoice){
+  //if playerChoice is the next item of computerChoice in choice array then player wins
+  else if (playerChoice === choice[(choice.indexOf(computerChoice) + 1) % 3]){
     message.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
     playerScoreDisplay.textContent = ++playerScore;
-  }
-
-  else {
-    message.textContent =`You Lose! ${computerChoice} beats ${playerChoice}`;
+  } else {
+    message.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
     computerScoreDisplay.textContent = ++computerScore;
   }
-
   //shows playButton after the round ends
   isEndRound = true;
   playButton.style.display = 'inline';
-  
-  setTimeout(function(){
-    if(checkWinner()) resetGame();
+  setTimeout(function () {
+    if (checkWinner()) resetGame();
   }, 50);
 }
 
-function checkWinner(){
-  if (playerScore === 5){
+function checkWinner() {
+  if (playerScore === 5) {
     alert('Player wins the game!');
     return true;
-  }
-  else if (computerScore === 5){
+  } else if (computerScore === 5) {
     alert('Computer wins the game!');
     return true;
   }
+  return false;
 }
 
-function resetGame(){
+function resetGame() {
   playerScore = 0;
   computerScore = 0;
   isFirstRound = true;
@@ -100,7 +91,7 @@ function resetGame(){
   playButton.textContent = 'Play Again!';
 }
 
-function resetUI(){
+function resetUI() {
   playerScoreDisplay.textContent = playerScore;
   computerScoreDisplay.textContent = computerScore;
   message.textContent = 'Select your choice';
